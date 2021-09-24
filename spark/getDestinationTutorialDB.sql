c94f0b27180cd0ebbcb53bd1109a7dc9ac6d7215
apano-on <albulen.pano@unibz.it>
2021-09-24 02:55:52 +0200

    First working version

--- spark/getDestinationTutorialDB.sql
+++ spark/getDestinationTutorialDB.sql
@@ -1,12 +1,31 @@
-CREATE TEMPORARY VIEW jdbcTable
-USING org.apache.spark.sql.jdbc
-OPTIONS (
-  url "jdbc:postgresql:dbserver",
-  dbtable "source1.municipalities",
-  user 'postgres',
-  password 'postgres2'
-)
+CREATE SCHEMA if not exists source1;
 
-INSERT INTO TABLE jdbcTable
-SELECT * FROM resultTable
+DROP TEMPORARY VIEW IF EXISTS jdbcTable;
+CREATE TEMPORARY VIEW jdbcTable
+ USING org.apache.spark.sql.jdbc
+ OPTIONS (
+   driver "org.postgresql.Driver",
+   url "jdbc:postgresql://db:5432/",
+   dbtable "source1.municipalities",
+   user 'postgres',
+   password 'postgres2'
+ );
+
+CREATE TABLE source1.municipalities(
+    m_id STRING NOT NULL,
+    istat STRING NOT NULL,
+    name_en STRING NOT NULL,
+    name_it STRING NOT NULL,
+    name_de STRING NOT NULL,
+   `population` INTEGER,
+    latitude DOUBLE NOT NULL,
+    longitude DOUBLE NOT NULL,
+    altitude DOUBLE NOT NULL,
+    geometryPoint STRING,
+    istat STRING
+    );
+
+INSERT INTO TABLE source1.municipalities
+SELECT * FROM jdbcTable;
+
 
